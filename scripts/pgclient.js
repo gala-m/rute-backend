ca60ecedab2ac87a06d41c7a5fcd707b40a41634
Gala <gala.morake@gmail.com>
2022-01-19 19:42:38 +0200

    host

--- scripts/pgclient.js
+++ scripts/pgclient.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader({
         'content-type': 'text/plain', 
-        'host': 'localhost', 
+        'host': 'https://rute-map.herokuapp.com', 
         'accept': '*'
     });
     res.end('Hello World');
